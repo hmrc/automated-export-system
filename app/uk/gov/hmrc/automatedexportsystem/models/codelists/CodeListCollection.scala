7017ac684ca098a71dd96f7b211e8ed84763da98
Kat <gravesmorgan02@gmail.com>
2026-06-26 12:26:53 +0100

    Added code list collection models for AES395.

---
+++ app/uk/gov/hmrc/automatedexportsystem/models/codelists/CodeListCollection.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+
+package uk.gov.hmrc.automatedexportsystem.models.codelists
+
+case class MessageTypeCodeList(
+  values: Seq[CodeList]
+)
+
+case class TypeOfLocationCodeList(
+  values: Seq[CodeList]
+)
+
+case class NationalityCodeList(
+  values: Seq[CodeList]
+)
+
+case class TransportModeCodeList(
+  values: Seq[CodeList]
+)
+
+case class CustomsOfficeExitCodeList(
+  values: Seq[CodeList]
+)
