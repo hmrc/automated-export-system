0fddf9d990a1f8eeddf8c20e7e8db56b0563b342
tudorsonycx <79589391+tudorsonycx@users.noreply.github.com>
2026-06-26 16:48:15 +0100

    AES-233: refactor build definition

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,23 +3,21 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val playVersion = "play-30"
+  private val playVersion      = "play-30"
   private val hmrcMongoVersion = "2.12.0"
 
-  val compile = Seq(
-    "uk.gov.hmrc"            %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"       %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion
   )
 
-  val test = Seq(
-    "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion"    % bootstrapVersion     % "test,it",
-    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-$playVersion"   % hmrcMongoVersion     % "test,it",
-
-    "org.scalatest"     %% "scalatest"          % "3.2.20" % "test,it",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.2" % "test,it",
-
-    "org.scalacheck"    %% "scalacheck"         % "1.19.0" % "test",
-    "org.scalatestplus" %% "scalacheck-1-17"    % "3.2.18.0" % "test"
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion"  % bootstrapVersion % Test,
+    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion % Test,
+    "org.scalatest"          %% "scalatest"                     % "3.2.20"         % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"            % "7.0.2"          % Test,
+    "org.scalacheck"         %% "scalacheck"                    % "1.19.0"         % Test,
+    "org.scalatestplus"      %% "scalacheck-1-17"               % "3.2.18.0"       % Test
   )
 
 }
