1a63c6ca3964f6c709c8b2bd445bc6b2151c0717
Kat <gravesmorgan02@gmail.com>
2026-07-13 11:45:02 +0100

    Tested controller.

---
+++ test/uk/gov/hmrc/automatedexportsystem/controllers/IE507StubControllerSpec.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.automatedexportsystem.controllers
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import play.api.test.Helpers.stubControllerComponents
+
+class IE507StubControllerSpec extends AnyWordSpec with Matchers {
+
+  private val controller =
+    new IE507StubController(
+      stubControllerComponents()
+    )
+
+  "IE507StubController" should {
+
+    "return NoContent for a valid POST request" in {
+
+      val result =
+        controller.submit()(
+          FakeRequest(POST, "/cds/aesIE507Request/v1")
+        )
+
+      status(result)          shouldBe NO_CONTENT
+      contentAsString(result) shouldBe ""
+    }
+  }
+
+  "serve POST /cds/aesIE507Request/v1" in {
+
+    val app = new GuiceApplicationBuilder().build()
+
+    running(app) {
+
+      val request =
+        FakeRequest(
+          POST,
+          "/automated-export-system/cds/aesIE507Request/v1"
+        )
+
+      val Some(result) =
+        route(app, request)
+
+      status(result) shouldBe NO_CONTENT
+    }
+  }
+}
