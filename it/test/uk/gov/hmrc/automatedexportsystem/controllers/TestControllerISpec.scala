19bcb9cdda5f6f6f1146dc5155c26d4681585261
abigail-hmrc <abigail.houston1@digital.hmrc.gov.uk>
2026-09-04 14:39:40 +0100

    AES-820: Delete end point for testing (#22)
    * Removed the test prefix from IBaseSpec as causing compile issues locally
    
    * Added service and controller
    
    * Added service and controller
    
    * completed the ITs etc
    
    * Removed the test prefix from IBaseSpec as causing compile issues locally
    
    # Conflicts:
    #	it/test/uk/gov/hmrc/automatedexportsystem/controllers/NotificationControllerISpec.scala
    #	it/test/uk/gov/hmrc/automatedexportsystem/controllers/SubmissionControllerITSpec.scala
    #	it/test/uk/gov/hmrc/automatedexportsystem/controllers/parsers/XmlBodyParsersITSpec.scala
    
    * Added service and controller
    
    * Added service and controller
    
    * completed the ITs etc
    
    * Rebased to fix merge conflicts
    
    * Update app/uk/gov/hmrc/automatedexportsystem/services/test/TestService.scala
    
    Co-authored-by: David Gregory <2992938+DavidGregory084@users.noreply.github.com>
    
    * Updated as per PR comments
    
    * Updated as per PR comments
    
    ---------
    
    Co-authored-by: David Gregory <2992938+DavidGregory084@users.noreply.github.com>

---
+++ it/test/uk/gov/hmrc/automatedexportsystem/controllers/TestControllerISpec.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.automatedexportsystem.controllers
+
+import org.mockito.Mockito.when
+import org.mongodb.scala.SingleObservableFuture
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalatest.time.{Seconds, Span}
+import play.api.http.Status.NO_CONTENT
+import play.api.mvc.ControllerComponents
+import play.api.test.{FakeRequest, Helpers}
+import uk.gov.hmrc.automatedexportsystem.config.AppConfig
+import uk.gov.hmrc.automatedexportsystem.controllers.test.TestController
+import uk.gov.hmrc.automatedexportsystem.generators.MongoAesIE507MessageGenerator
+import uk.gov.hmrc.automatedexportsystem.helpers.BaseISpec
+import uk.gov.hmrc.automatedexportsystem.models.mongo.write.MongoAesIE507Message
+import uk.gov.hmrc.automatedexportsystem.repositories.AesIE507RepositoryImpl
+import uk.gov.hmrc.automatedexportsystem.services.test.TestService
+
+import scala.concurrent.ExecutionContext
+
+class TestControllerISpec extends BaseISpec, MongoAesIE507MessageGenerator:
+  val repository: AesIE507RepositoryImpl = app.injector.instanceOf[AesIE507RepositoryImpl]
+
+  override def beforeEach(): Unit =
+    super.beforeEach()
+    await(repository.collection.drop().head())
+
+  trait Setup:
+    val appConfig: AppConfig = mock[AppConfig]
+    when(appConfig.replaceIndexes).thenReturn(true)
+
+    val service    = new TestService(repository)
+    val controller = new TestController(app.injector.instanceOf[ControllerComponents], service)
+
+  given ec: ExecutionContext = ExecutionContext.global
+
+  implicit val patience: PatienceConfig = PatienceConfig(timeout = scaled(Span(10, Seconds)))
+
+  "TestController.deleteAll" - {
+    "should return NoContent when deletion is successful" in new Setup {
+
+      val testMessages = Gen.listOfN(3, arbitrary[MongoAesIE507Message]).sample.get
+      testMessages.foreach { msg =>
+        repository.submit(msg).value.futureValue shouldBe a[Right[_, _]]
+      }
+
+      repository.collection.countDocuments().toFuture().futureValue shouldBe 3L
+
+      val result = controller.deleteAll()(FakeRequest())
+
+      Helpers.status(result)                                        shouldBe NO_CONTENT
+      repository.collection.countDocuments().toFuture().futureValue shouldBe 0L
+    }
+
+  }
---
+++ it/test/uk/gov/hmrc/automatedexportsystem/services/test/TestServiceISpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.automatedexportsystem.services.test
+
+import uk.gov.hmrc.automatedexportsystem.config.AppConfig
+import uk.gov.hmrc.automatedexportsystem.helpers.BaseISpec
+import org.mockito.Mockito.when
+import org.mongodb.scala.SingleObservableFuture
+import org.scalacheck.Gen
+import uk.gov.hmrc.automatedexportsystem.generators.MongoAesIE507MessageGenerator
+import uk.gov.hmrc.automatedexportsystem.models.mongo.write.MongoAesIE507Message
+import uk.gov.hmrc.automatedexportsystem.repositories.AesIE507Repository
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalatest.concurrent.IntegrationPatience
+
+import scala.concurrent.ExecutionContext
+
+class TestServiceISpec extends BaseISpec with MongoAesIE507MessageGenerator with IntegrationPatience:
+
+  given ec:      ExecutionContext = ExecutionContext.global
+  val appConfig: AppConfig        = mock[AppConfig]
+  when(appConfig.replaceIndexes).thenReturn(true)
+
+  extension (mongoAesIE507MessageGen: Gen[MongoAesIE507Message])
+    def getMessage: Gen[MongoAesIE507Message] =
+      mongoAesIE507MessageGen
+
+  "TestService.deleteAll" - {
+    "delete all records and recreate indexes" in {
+
+      val repository = app.injector.instanceOf[AesIE507Repository]
+      val service    = new TestService(repository)
+
+      val testMessages = Gen.listOfN(3, arbitrary[MongoAesIE507Message]).sample.get
+      testMessages.foreach { msg =>
+        repository.submit(msg).value.futureValue shouldBe a[Right[_, _]]
+      }
+
+      repository.collection.countDocuments().toFuture().futureValue shouldBe 3L
+      service.deleteAll.value.futureValue                           shouldBe Right(())
+
+      repository.collection.countDocuments().toFuture().futureValue shouldBe 0L
+
+      val indexInfo = repository.ensureIndexes().futureValue
+      indexInfo should not be empty
+    }
+  }
